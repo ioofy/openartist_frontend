04a09534592e1e468fd0fee95fc6410f008834ec
ioofy <mynev.id@gmail.com>
2022-05-01 16:31:08 +0700

    feat: setup husky and configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p>We will change this in the future, awesome</p>
+      <p>We will change this in the future, hopefully</p>
     </div>
   );
 }
