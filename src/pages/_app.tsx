ecda67ff9660a6a52f8bafc6caf2e3d01d68690b
ioofy <mynev.id@gmail.com>
2022-05-02 08:35:49 +0700

    feat: adding layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
 import { globalStyles } from '@/styles/global.styles';
+import Layout from '@/layout/Layout';
 import '@/styles/css/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
